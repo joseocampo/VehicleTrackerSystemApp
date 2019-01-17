536f2c644659f72f9b4675bd36164743df5a3876
Moises Moraga <moiso1908@gmail.com>
2019-01-16 22:57:42 -0600

    cambios en vista main

--- app/src/main/java/joseocampo/VehicleTrackerSystemApp/com/MainActivity.java
+++ app/src/main/java/joseocampo/VehicleTrackerSystemApp/com/MainActivity.java
@@ -4,18 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_login;
+    EditText et_id, et_pass;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void iniciarSesion(View view){
-        Intent intent = new Intent(this,PantallaPrincipal.class);
-        startActivity(intent);
+
+        btn_login= findViewById(R.id.btn_login);
+        et_id= findViewById(R.id.et_identification);
+        et_pass= findViewById(R.id.et_password);
 
     }
 
