aa5472611ff5ae617a9d29a19dfaae7238e138ad
mGreco19 <moiso1908@gmail.com>
2019-01-16 21:43:13 -0600

    cambios en el activity_main

--- app/src/main/java/joseocampo/VehicleTrackerSystemApp/com/MainActivity.java
+++ app/src/main/java/joseocampo/VehicleTrackerSystemApp/com/MainActivity.java
@@ -4,14 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_login;
+    EditText et_id, et_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login= findViewById(R.id.btn_login);
+        et_id= findViewById(R.id.et_identification);
+        et_pass= findViewById(R.id.et_password);
+
+        //hacer loginRequest para el envio de datos
+
     }
     public void iniciarSesion(View view){
         Intent intent = new Intent(this,PantallaPrincipal.class);
